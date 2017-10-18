8502af03fab8c7c156140d4980897627fc1913aa
Cyril CHAPON <cyril.chapon@live.com>
2017-10-18 18:29:16 +0200

    Continue graceful error managmeent

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var boom = require('boom');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,6 +19,7 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -29,9 +31,7 @@ app.use('/transactions', transactions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(boom.notFound());
 });
 
 // error handler
