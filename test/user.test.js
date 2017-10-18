377247eb12166f16c9f264f319c0e99197b07d18
Cyril CHAPON <cyril.chapon@live.com>
2017-10-18 11:12:06 +0200

    Add test for give() method

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,42 @@
 const User = require('../models').user;
+const Moniker = require('moniker');
+const Promise = require('bluebird');
+const faker = require('faker');
+const _ = require('lodash');
+
+function getRandomUserValues(values) {
+  return _.merge({
+    username: Moniker.choose(),
+    balance: faker.random.number()
+  }, values);
+}
 
 test('A user has a default balance of 0', () => {
   const user = User.build();
   expect(user.balance).toEqual(0);
+});
+
+describe('.give()', function() {
+  test('A give() increments and decrements', () => {
+    let amount = 3;
+
+    const initialUserFromData = getRandomUserValues();
+    const initialUserToData = getRandomUserValues();
+
+    return Promise.all([
+      User.create(_.cloneDeep(initialUserFromData)),
+      User.create(_.cloneDeep(initialUserToData))
+    ])
+    .spread(function(userFrom, userTo) {
+      return Promise.all([
+        userFrom,
+        userTo,
+        userFrom.give(amount, userTo)
+      ]);
+    })
+    .spread(function(userFrom, userTo) {
+      expect(userFrom.balance).toEqual(initialUserFromData.balance - amount);
+      expect(userTo.balance).toEqual(initialUserToData.balance + amount);
+    });
+  });
 });
