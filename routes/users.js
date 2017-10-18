4ac1c6112d051bb6722d057bf6a5f7e36c9060b9
Cyril CHAPON <cyril.chapon@live.com>
2017-10-18 15:57:14 +0200

    Simple user listing and creation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const models = require('../models');
+const Sequelize = models.Sequelize;
+const User = models.user;
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.findAll({raw: true})
+  .then(function(users) {
+    res.render('users', { users: users });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  User.create(req.body)
+  .then(function() {
+    res.redirect('/users');
+  })
+  .catch(next);
 });
 
 module.exports = router;
